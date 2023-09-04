1441a1e0037f2df4cd71626f9f41e9508d93df92
alumno <alumno@DESKTOP-N2CU3S2>
2023-09-04 20:45:42 -0300

    Agregar archivos de proyecto.

---
+++ lolo2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lolo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public String texto = "";
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                texto = texto + " Nombre: " + nom.Text + "      Cantidad:" + Cantidad.Text + "\n";
+                label1.Text = "Lista: "+texto;
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+    }
+}
